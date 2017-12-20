df810a609a211004e8dfc78b3baf2b4e2d01ed82
Unknown <yarden.a.a@gmail.com>
2017-12-20 16:52:39 +0200

    Q3 simulink files + parameters definition

--- Control System Design/Homework 1/CSD_HW_1_Script.m
+++ Control System Design/Homework 1/CSD_HW_1_Script.m
@@ -5,3 +5,18 @@ c = 2;
 a = 0.1e-6;
 V_max = 100;
 L = 100e-6;
+
+
+%% Q3 - Servo motor control
+
+Kt   = 0.03;       % Nm/A
+R    = 0.1;        % Ohm
+L    = 0;          % H
+Vmax = 1;          % V
+Jm   = 0.001;      % Kgm^2 
+Jl   = 0.02;       % Kgm^2
+c    = 0.001;      % Nmsec/rad
+N    = 3;
+K    = [0.1 30];   % Nm/rad
+d    = 5*pi/180;   % rad
+
